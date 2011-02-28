89401d7ad5ead741f9543d4839e3df4c813ccdbb
Paul Connolley <shunuk@gmail.com>
2011-02-28 21:32:09 +0000

    Skinny AppController through Fat LenoreComponent! Moved much of the AppController methods to Lenore Component Tidied the ACL methods/actions to GroupsController

---
+++ app/controllers/components/lenore.php
@@ -0,0 +1,150 @@
+<?php
+class LenoreComponent extends Object {
+	var $Controller;
+
+	function initialize(&$controller) {
+		$this->Controller =& $controller;
+		$this->Controller->Auth->allowedActions = array('display','login','logout','index','view','add');
+		//$this->Controller->Auth->allowedActions = array('*');
+		$this->minifyJs();
+		$this->minifyCss();
+	}
+
+	function startup(&$controller, $settings=array()) {
+		$this->setCurrentTheme();
+		$this->setModDateHeader();
+		$this->setAltContentViewParams();
+		$this->setRequestHandlerViewVars();
+		$this->customControllerViewData();
+		$this->mergeGetDataWithThisData();
+		$this->setSnippets();
+		$this->getInboxMessages();
+	}
+
+	function mergeGetDataWithThisData() {
+		if(!empty($this->Controller->params['url']['data'])) {
+			if(empty($this->Controller->data)) {
+				$this->Controller->data = $this->Controller->params['url']['data'];
+			} else {
+				$this->Controller->data = array_merge($this->Controller->params['url']['data'],$this->Controller->data);
+			}
+			unset($this->Controller->params['url']['data']);
+		}
+	}
+
+	function minifyCss() {
+		if($this->actionIsAdmin()) {
+			$this->Controller->set('minify_css',$this->Controller->Minify->css(array(
+				'css/admin/reset.css','css/admin/typefaces.css','css/admin/lenore.css','css/admin/widgets/hook_menu.css',
+				'css/admin/widgets/sortable.css','css/admin/widgets/editable_text.css','css/admin/widgets/flag_toggle.css',
+				'css/admin/widgets/dialog.css','css/admin/widgets/flash_messages.css','css/admin/widgets/resource_list.css',
+				'css/admin/widgets/paginate.css','css/admin/handheld_large.css','css/admin/tablet_netbooks.css','css/admin/desktop.css'
+			)));
+		} elseif($css_paths = Configure::read('Minify.public.css')) {
+			$pub_minify_css = array();
+			foreach($css_paths as $t=>$css) {
+				$pub_minify_css[$t] = $this->Controller->Minify->css($css);
+			}
+			$this->Controller->set('pub_minify_css',$pub_minify_css);
+		}
+	}
+
+	function minifyJs() {
+		if($this->actionIsAdmin()) {
+			$this->Controller->set('min_js_head',$this->Controller->Minify->js(array(
+				'js/lib/modernizr.js','js/lib/selectivizr.js','js/lib/flexie.js','js/lib/mediaqueries.js','js/jquery/jquery.js','js/jquery/ui/core.js',
+				'js/jquery/ui/widget.js','js/jquery/ui/mouse.js', 'js/jquery/ui/bgiframe.js','js/jquery/ui/sortable.js','js/jquery/ui/dialog.js',
+				'js/jquery/ui/position.js','js/jquery/lib/iphoneui.js','js/jquery/lib/editable_text.js','js/jquery/hook_menu.js','js/jquery/dux_tabs.js',
+				'js/jquery/lib/flag_toggle.js','js/lib/ckeditor/lenore_load.js','dontpack1'=>'js/lib/ckeditor/ckeditor.js','dontpack2'=>'js/lib/ckeditor/adapters/jquery.js','js/admin/load_config.js',
+				'js/lib/underscore.js','js/lib/backbone.js','js/lib/paginatedcollection.backbone.js',
+				'js/admin/app.js','js/admin/models/message.js','js/admin/collections/message_collection.js','js/admin/views/message_view.js'
+			)));
+		} elseif($js_paths = Configure::read('Minify.public.js')) {
+			$pub_minify_js = array();
+			foreach($js_paths as $t=>$js) {
+				$pub_minify_js[$t] = $this->Controller->Minify->js($js);
+			}
+			$this->Controller->set('pub_minify_js',$pub_minify_js);
+		}
+	}
+
+	function customControllerViewData() {
+		$this_model = Inflector::singularize($this->Controller->name);
+		$this_action = $this->Controller->action;
+		if(($action_data=Configure::read("$this_model.action_data.$this_action")) && is_array($action_data)) {
+			foreach($action_data as $key=>$action) {
+				$set = $key;
+				$model = $action['model'];
+				$type = $action['type'];
+				$query['conditions'] = $action['conditions'];
+				if(!empty($action['order'])) $query['order'] = $action['order'];
+				if(!empty($action['limit'])) $query['limit'] = $action['limit'];
+				if(!empty($action['fields'])) $query['fields'] = $action['fields'];
+				if(!empty($action['recursive'])) $query['recursive'] = $action['recursive'];
+				if($this->Controller->loadModel($model)) {
+					$this->Controller->set($set,$this->Controller->{$model}->find($type,$query));
+				}
+			}
+		}
+	}
+
+	function setAltContentViewParams() {
+		if(isset($this->Controller->params['alt_content']) && $this->Controller->params['alt_content']=='Rss') {
+			$this->Controller->RequestHandler->renderAs($this,'rss');
+			$this->Controller->RequestHandler->respondAs('Content-type: application/rss+xml;charset=UTF-8');
+		} elseif(isset($this->Controller->params['alt_content']) && $this->Controller->params['alt_content']=='Xml') {
+			$this->Controller->RequestHandler->renderAs($this,'xml');
+			$this->Controller->RequestHandler->respondAs('xml');
+		}
+	}
+
+	function setRequestHandlerViewVars() {
+		if($this->Controller->RequestHandler->isAjax())
+			$this->Controller->set('is_ajax',true);
+		if($this->Controller->RequestHandler->isMobile())
+			$this->Controller->set('is_mobile',true);
+		else
+			$this->Controller->set('is_mobile',false);
+	}
+
+	function setModDateHeader() {
+		if(!empty($this->Controller->viewVars['mod_date_for_layout'])) $this->Controller->header('Last-Modified: '.$this->Controller->Time->toRSS($this->Controller->viewVars['mod_date_for_layout']));
+	}
+
+	function setCurrentTheme() {
+		if($this->actionIsAdmin()) {
+			$this->Controller->theme = 'admin';
+		} elseif($template_theme = Configure::read('Moonlight.template_theme')) {
+			$this->Controller->theme = $template_theme;
+			if($custom_layout = Configure::read(Inflector::singularize($this->Controller->name).'.custom_layout')) {
+				$this->Controller->layout = $custom_layout;
+			}
+		}
+	}
+
+	function setSnippets() {
+		if(!$this->actionIsAdmin()) {
+			if(false===($snippets=Cache::read('snippets_f'))) {
+				$snippets = $this->Controller->Snippet->find('list',array('fields'=>array('Snippet.slug','Snippet.content')));
+				Cache::write('snippets_f',$snippets);
+			}
+			$this->Controller->set('snippets',$snippets);
+		}
+	}
+	
+	function actionIsAdmin() {return !empty($this->Controller->params['prefix']) && $this->Controller->params['prefix'] == 'admin';}
+	
+	function getInboxMessages() {
+		if($this->actionIsAdmin()) {
+			if(false===($msg_cache=Cache::read('messages_a'))) {
+				$messages = $this->Controller->paginate('Message');
+				$msg_cache = array('results' => $messages,'paging' => $this->Controller->params['paging']['Message']);
+				Cache::write('messages_a',$msg_cache);
+			} else {
+				$messages = $msg_cache['results'];
+				$this->Controller->params['paging']['Message'] = $msg_cache['paging'];
+			}
+			$this->Controller->set('messages_a',$messages);
+		}
+	}
+}
--- app/controllers/components/menu.php
+++ app/controllers/components/menu.php
@@ -1,12 +1,12 @@
 <?php
 class MenuComponent extends Object {
 
-	var $controller;
+	var $Controller;
 
 	function startup(&$controller) {
 		if(empty($controller->plugin)) {
-			$this->controller = $controller;
-			if($this->controller->actionIsAdmin()) {
+			$this->Controller = $controller;
+			if($this->Controller->Lenore->actionIsAdmin()) {
 				$this->setAdminMenu();
 			} else {
 				$this->setMenus();
@@ -20,17 +20,17 @@ class MenuComponent extends Object {
 		foreach($enabled_modules as $x=>$module) $admin_menu['admin/'.Inflector::tableize($x)] = Inflector::pluralize($module['alias']);
 		$admin_menu['admin/snippets'] = 'Snippets';
 		$admin_menu['http://moonlight-project.co.uk/help'] = 'Help';
-		$this->controller->set('admin_menu',$admin_menu);
+		$this->Controller->set('admin_menu',$admin_menu);
 	}
 
 	function setMenus() {
-		$this->controller->set('menu_prefix',Configure::read('Menu.prefix'));
-		$this->controller->set('menu_suffix',Configure::read('Menu.suffix'));
-		$this->controller->set('menu_omissions',Configure::read('Menu.omissions'));
-		$this->controller->set('moonlight_website_menu',$this->controller->Section->find('list',array(
+		$this->Controller->set('menu_prefix',Configure::read('Menu.prefix'));
+		$this->Controller->set('menu_suffix',Configure::read('Menu.suffix'));
+		$this->Controller->set('menu_omissions',Configure::read('Menu.omissions'));
+		$this->Controller->set('moonlight_website_menu',$this->Controller->Section->find('list',array(
 			'fields'=>array('Section.slug','Section.title')
 		)));
-		$this->controller->set('moonlight_product_list',$this->controller->Category->find('list',array(
+		$this->Controller->set('moonlight_product_list',$this->Controller->Category->find('list',array(
 			'fields'=>array('Category.slug','Category.title'),
 			'conditions'=>array('Category.category_id'=>null,'Category.draft'=>false)
 		)));
