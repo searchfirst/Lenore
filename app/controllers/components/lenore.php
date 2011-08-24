a65d2f792c97deac5ffa5090f3c5d5c15192d4a1
Paul Connolley <shunuk@gmail.com>
2011-08-24 14:44:24 +0100

    Fix error caused by empty array not being initialised for snippets

--- app/controllers/components/lenore.php
+++ app/controllers/components/lenore.php
@@ -130,6 +130,7 @@ class LenoreComponent extends Object {
 		if(!$this->actionIsAdmin()) {
 			if(false===($snippets=Cache::read('snippets_f','lenore'))) {
 				$snippets = $this->Controller->Snippet->find('list',array('fields'=>array('Snippet.slug','Snippet.content')));
+				$rSnippets = array();
 				foreach ($snippets as $k => $snippet) {
 					$rSnippets[str_replace('-','_',$k)] = $snippet;
 				}
